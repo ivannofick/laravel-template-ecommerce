6c96a3c075c3f65520876f86bca7a05415fc6be6
ivannofick@gmail.com <ivannofick.nugraha@mncgroup.com>
2023-09-02 00:49:38 +0700

    feat: add ajax on homepage

--- resources/views/dashboard/homepage.blade.php
+++ resources/views/dashboard/homepage.blade.php
@@ -5,7 +5,7 @@
 <div class="p-24">
     <div class="text-stone-900 text-2xl font-bold pb-12">Product Tersedia</div>
     <div class="w-44 h-64 bg-white shadow border border-neutral-300">
-        <img class="w-full h-40" src="https://via.placeholder.com/112x151" />
+        <img class="w-full h-40" src="{{ asset('assets/img/parfum.svg') }}" />
         <div class="w-full p-4 h-[94px] mt-4">
             <p class="text-stone-900 text-sm font-bold leading-none">Euodia</p>
             <p class="text-blue-500 text-sm font-bold">IDR x.xxx.980</p>
@@ -13,5 +13,20 @@
     </div>
 </div>
 @include('dashboard.atom.footers')
+<script type="text/javascript">
+        $.ajax({
+            url: "{{ env('API_URL') }}/product/data?skip=0&take=5",
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            type: "GET", /* or type:"GET" or type:"PUT" */
+            success: function (data) {
+                if (data.code.status === 0) {
+                    const response = data.data;
+                    console.log(response) 
+                }
 
+            }
+        });
+</script>
 @endsection
--- resources/views/template/head.blade.php
+++ resources/views/template/head.blade.php
@@ -1,4 +1,8 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<title>Bootstrap demo</title>
+<meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
+
+<title>AwComerce</title>
 <script src="https://cdn.tailwindcss.com"></script>
+{{-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script> --}}
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.10.1/jquery.min.js" integrity="sha512-eH6+OZuv+ndnPTxzVfin+li0PXKxbwi1gWWH2xqmljlTfO3JdBlccZkwWd0ZcWAtDTvsqntx1bbVSkWzHUtfQQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
