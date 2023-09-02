51a4ed9f89556e7c8f0cd33319cb0e065af37b6a
ivannofick@gmail.com <ivannofick.nugraha@mncgroup.com>
2023-09-02 17:39:15 +0700

    feat: add dropdown user on right position

--- resources/views/admin/atom/headers.blade.php
+++ resources/views/admin/atom/headers.blade.php
@@ -1,22 +1,49 @@
 <div class="w-full h-16 bg-white">
-    <div class="grid grid-cols-2 gap-2">
-        <div class="ml-[91px] pt-[20px]">
-            <div class="w-28 h-11 text-neutral-900 text-sm font-normal">
-                <img src="{{ asset('assets/img/logo.svg') }}" />
-            </div>
-        </div>
-        <div class="grid justify-items-end mr-[91px] pt-[20px]">
-            <a href="{{ route('dashboard-admin', ['name'=> 'aku']) }}" class="left-[122px] grid grid-cols-2 gap-2">
-                <div class="w-24 h-9">
-                  <div class="w-auto text-blue-500 text-xs font-normal">Hallo Admin,</div>
-                  <div class="w-auto text-black text-sm font-normal">Aden S. Putra</div>
-                </div>
-                <div class="w-auto h-10 px-4 left-[90px] top-0">
-                  <div class="w-10 h-10 bg-stone-300 rounded-full"></div>
-                </div>          
-        
-              </a>
-        </div>
+  <div class="grid grid-cols-2 gap-2">
+      <div class="ml-[91px] pt-[20px]">
+          <div class="w-28 h-11 text-neutral-900 text-sm font-normal">
+              <img src="{{ asset('assets/img/logo.svg') }}" />
+          </div>
+      </div>
+      <div class="grid justify-items-end mr-[91px] pt-[20px]">
 
-    </div>
+        <div class="relative inline-block text-right">
+          <div id="dropdownButton"  class="left-[122px] grid grid-cols-2 gap-2">
+            <div class="w-24 h-9">
+              <div class="w-auto text-blue-500 text-xs font-normal">Hallo Admin,</div>
+              <div class="w-auto text-black text-sm font-normal">Aden S. Putra</div>
+            </div>
+            <div class="w-auto h-10 px-4 left-[90px] top-0">
+              <div class="w-10 h-10 bg-stone-300 rounded-full"></div>
+            </div>          
+    
+          </div>
+
+          <div id="dropdownMenu" class="hidden absolute right-0 mt-2 w-48 bg-white border border-gray-200 rounded-md shadow-lg">
+            <div class="flex flex-col items-center px-4 py-2 border-b border-gray-200">
+              <div class="w-16 h-16 bg-stone-300 rounded-full mb-2"></div>
+              <span class="text-black text-sm font-normal">Aden S. Putra</span>
+              <span class="text-black text-xs font-normal">Aden@gmail.com</span>
+            </div>
+            <a href="#" class="block px-4 py-2 text-red-600 hover:bg-gray-100 text-center">Logout</a>
+          </div>
+        </div>
+        
+      </div>
+
+  </div>
 </div>
+
+<script>
+const dropdownButton = document.getElementById('dropdownButton');
+const dropdownMenu = document.getElementById('dropdownMenu');
+
+dropdownButton.addEventListener('click', function () {
+  if (dropdownMenu.classList.contains('hidden')) {
+    dropdownMenu.classList.remove('hidden');
+  } else {
+    dropdownMenu.classList.add('hidden');
+  }
+});
+</script>
+
